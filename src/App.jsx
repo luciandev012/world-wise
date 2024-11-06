643f27fc33ee3d58957f4277554cfb09c3cad062
Duong Van Thang <thang@192.168.1.23.non-exists.ptr.local>
2024-11-06 22:45:40 +0700

    add navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import HomePage from "./pages/HomePage";
 import Pricing from "./pages/Pricing";
@@ -8,6 +8,7 @@ import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://127.0.0.1:8000";
 export default function App() {
@@ -39,7 +40,7 @@ export default function App() {
         <Route path="/app" element={<AppLayout />}>
           <Route
             index
-            element={<CityList cities={cities} isLoading={isLoading} />}
+            element={<Navigate replace to="cities" />}
           />
           <Route
             path="cities"
@@ -50,7 +51,7 @@ export default function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,32 +1,12 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { useEffect, useRef } from "react";
-import ScrollVideo from "./ScrollVideo";
 
 export default function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
-  const videoRef = useRef(null);
-  const event = "mousemove";
 
-  videoRef?.current?.click;
-  useEffect(() => {
-    const handleUserInteraction = () => {
-      if (videoRef.current) {
-        videoRef.current.muted = false;
-        videoRef.current.play();
-      }
-      // Remove the event listener after it runs once
-      window.removeEventListener(event, handleUserInteraction);
-    };
+  const navigate = useNavigate();
 
-    // Add event listener for any user interaction (e.g., click)
-    window.addEventListener(event, handleUserInteraction);
-
-    return () => {
-      window.removeEventListener(event, handleUserInteraction);
-    };
-  }, []);
-  return <div className={styles.mapContainer}></div>;
+  return <div className={styles.mapContainer} onClick={() => navigate("form")}></div>;
 }
